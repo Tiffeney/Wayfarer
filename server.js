b677629aa0322368dd51614e9f22cc803d2853ae
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-05 23:30:18 -0700

    updated server pg with dependencies, added passport file and listening on port

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const
-    express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    logger = require('morgan')
-    PORT = 3000;
+express = require('express'),
+app = express(),
+ejsLayouts = require('express-ejs-layouts'),
+mongoose = require('mongoose'),
+flash = require('connect-flash'),
+logger = require('morgan'),
+cookieParser = require('cookie-parser'),
+session = require('express-session'),
+MongoDBStore = require('connect-mongodb-session')(session),
+passport = require('passport'),
+passportConfig = require('./config/passport'),
+PORT = 3000;
 
 app.use(express.json()),
 
 app.listen(PORT, err => {
-    console.log(err || `Server is running on port ${PORT}`);
+    console.log(err || `Server is listening on port ${PORT}`);
 })
