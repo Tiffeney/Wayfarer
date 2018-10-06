5396b8de1158454c069f5190489c967e55e79e8f
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-05 23:46:29 -0700

    created User.js /models/User.js, setup schema, required user file and bcrypt/hashing of password functionality

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const
+  mongoose = require('mongoose'),
+  bcrypt = require('bcrypt-nodejs'), //Step 1 hashing password
+  User = require('../models/User');
+  userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+  })
+      
+//Incrypting the password by using hashing      
+userSchema.methods.generateHash = function(password) { 
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+      
+userSchema.methods.isValidPassword = function(password) { //is what the user typed in the same as what is stored in the database.
+  return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.pre('save', function(next) { //Before saving
+  if (this.isModified('password')) {  //Check to see if the password was saved
+    this.password = this.generateHash(this.password)
+  };
+      // Next Step
+  next();
+})
+
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
