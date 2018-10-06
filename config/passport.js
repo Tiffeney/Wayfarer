309650afc0a3f72c6e96c8073ba5bd4321b16227
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-06 14:21:50 -0700

    added local signup and local login

--- config/passport.js
+++ config/passport.js
@@ -13,3 +13,35 @@ passport.deserializeUser((id, done) => {
         done(err, user);
     });
 });
+
+//LOCAL Signup
+passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, (req, email, password, done) => {
+    User.findOne({ email }, (err, user) => {
+        if (err) return done(err);
+        if (user) return done(null, false, req.flash('signupMessage', "Email already taken."));
+        User.create(req.body, (err, newUser) => {
+            if (err) return console.log(err);
+            return done(null, newUser, null);
+        })
+    })
+}));
+
+// LOCAL LOGIN
+passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: "password",
+    passReqToCallback: true
+}, (req, email, password, done) => {
+    User.findOne({ email }, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, req.flash('signupMessage', "User doesn't exist."));
+        if (!user.isValidPassword(password)) return done(null, false, req.flash('loginMessage', "Incorrect Password."));
+        return done(null, user);
+    })
+}))
+
+module.exports = passport;
