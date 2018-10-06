a495fb489ce5d96e5a7a7c45ff10575a74a68efa
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-06 13:02:08 -0700

    forgot to acquire in method-override

--- server.js
+++ server.js
@@ -10,9 +10,21 @@ session = require('express-session'),
 MongoDBStore = require('connect-mongodb-session')(session),
 passport = require('passport'),
 passportConfig = require('./config/passport'),
+methodOverride = require('method-override')
 PORT = 3000;
 
 app.use(express.json()),
+
+
+// environment port
+// const
+// 	port = process.env.PORT || 3000,
+// 	mongoConnectionString = process.env.MONGODB_URI || 'mongodb://localhost/wayarer'
+
+// // mongoose connection
+// mongoose.connect(mongoConnectionString, (err) => {
+// 	console.log(err || "Connected to MongoDB (passport-authentication)")
+// })
 
 app.listen(PORT, err => {
     console.log(err || `Server is listening on port ${PORT}`);
