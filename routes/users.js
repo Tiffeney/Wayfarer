d44fad1864fdf39acc6c34ba6a697bc5fc618781
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-06 17:59:38 -0700

    fixed errors

--- routes/users.js
+++ routes/users.js
@@ -24,40 +24,40 @@ usersRouter.post('/signup', passport.authenticate('local-signup', { //Step 4
 }));
 
 
-// usersRouter.get('/profile', isLoggedIn, (req, res) => { //Step 2
-//   //Renders the user profile, only if they are currently login
-//   res.render('profile', { user: req.user });
-// });
+usersRouter.get('/profile', isLoggedIn, (req, res) => { //Step 2
+  //Renders the user profile, only if they are currently login
+  res.render('profile', { user: req.user });
+});
 
-// usersRouter.patch('/profile', isLoggedIn, (req, res) => {
-// // checking to see if the request body has a truthy password (meaning they are trying to change their password)
-// // If not, remove it from the rewuest body so that our app doesn't try to has an empty password string.
-//   if (!req.body.password) delete req.body.password;
-//   //Take the remaining key/value pairs from the request body, and merge them into the current object.
-//   Object.assign(req.user, req.body);
-//   req.user.save((err, updatedUser) => {
-//     if (err) return console.log(err);
-//     res.redirect('/users/profile');
-//   })
-// })
+usersRouter.patch('/profile', isLoggedIn, (req, res) => {
+// checking to see if the request body has a truthy password (meaning they are trying to change their password)
+// If not, remove it from the rewuest body so that our app doesn't try to has an empty password string.
+  if (!req.body.password) delete req.body.password;
+  //Take the remaining key/value pairs from the request body, and merge them into the current object.
+  Object.assign(req.user, req.body);
+  req.user.save((err, updatedUser) => {
+    if (err) return console.log(err);
+    res.redirect('/users/profile');
+  })
+})
 
-// usersRouter.get('/profile/edit', isLoggedIn, (req, res) => {
-//   res.render('editProfile');
-// })
+usersRouter.get('/profile/edit', isLoggedIn, (req, res) => {
+  res.render('editProfile');
+})
 
-// usersRouter.get('/logout', (req, res) => { //Step 3
-//   //Destroy the session, and redirect the user back to the homepage
-//   req.logout();
-//   res.redirect('/');
-// })
+usersRouter.get('/logout', (req, res) => { //Step 3
+  //Destroy the session, and redirect the user back to the homepage
+  req.logout();
+  res.redirect('/');
+})
 
-//   //A method used to authorize a user BEFORE allowing them to proceed to the profile page. Adding Middleware
-// function isLoggedIn(req, res, next) { //Step 1
-//   //If the user is authenticated, proceed to the requested route
-//   if (req.isAuthenticated()) return next()
-//   //Otherwise, return them to the login page
-//   res.redirect('/users/login');
-// }
+  //A method used to authorize a user BEFORE allowing them to proceed to the profile page. Adding Middleware
+function isLoggedIn(req, res, next) { //Step 1
+  //If the user is authenticated, proceed to the requested route
+  if (req.isAuthenticated()) return next()
+  //Otherwise, return them to the login page
+  res.redirect('/users/login');
+}
 
 
 
