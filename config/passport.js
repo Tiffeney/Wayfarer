974a8bdbeded7bd47e792c00e9e667a7e89abfb8
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-06 17:35:52 -0700

    not sure

--- config/passport.js
+++ config/passport.js
@@ -1,47 +1,43 @@
-const
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    User = require('../models/User');
+const passport = require('passport'),
+     LocalStrategy = require('passport-local').Strategy,
+     User = require('../models/User');
 
-    // creating a session key cookie
 passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-    
-passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-        done(err, user);
-    });
+   done(null, user.id);
 });
 
-//LOCAL Signup
+passport.deserializeUser((id, done) => {
+   User.findById(id, (err, user) => {
+       done(err, user);
+   });
+});
+
 passport.use('local-signup', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
+   usernameField: 'email',
+   passwordField: 'password',
+   passReqToCallback: true
 }, (req, email, password, done) => {
-    User.findOne({ email }, (err, user) => {
-        if (err) return done(err);
-        if (user) return done(null, false, req.flash('signupMessage', "Email already taken."));
-        User.create(req.body, (err, newUser) => {
-            if (err) return console.log(err);
-            return done(null, newUser, null);
-        })
-    })
+   User.findOne({ email }, (err, user) => {
+       if (err) return done(err);
+       if (user) return done(null, false, req.flash('signupMessage', "Email already used." ));
+       User.create(req.body, (err, newUser) => {
+           if (err) return console.log(err);
+           return done(null, newUser, null);
+       })
+   })
 }));
 
-// LOCAL LOGIN
 passport.use('local-login', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: "password",
-    passReqToCallback: true
+   usernameField: 'email',
+   passwordField: "password",
+   passReqToCallback: true
 }, (req, email, password, done) => {
-    User.findOne({ email }, (err, user) => {
-        if (err) return done(err);
-        if (!user) return done(null, false, req.flash('signupMessage', "User doesn't exist."));
-        if (!user.isValidPassword(password)) return done(null, false, req.flash('loginMessage', "Incorrect Password."));
-        return done(null, user);
-    })
+   User.findOne({ email }, (err, user) => {
+       if (err) return done(err);
+       if (!user) return done(null, false, req.flash('loginMessage', "User does not exist" ));
+       if (!user.isValidPassword(password)) return done(null, false, req.flash('loginMessage', "Incorrect Password" ));
+       return done(null, user);
+   })
 }))
 
 module.exports = passport;
